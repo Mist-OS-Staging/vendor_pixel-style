9b20c7aca41c9dd18a60560b7cd991abf199a51a
Zabuka_zuzu <zuzu950444@gmail.com>
2026-07-31 14:27:19 +0000

    venodor: init Mist A17

--- config/common.mk
+++ config/common.mk
@@ -90,11 +90,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.error.receiver.system.apps=com.google.android.gms \
     ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent
 
-# New charging information
-PRODUCT_PRODUCT_PROPERTIES += \
-    charging_string.apply_lotx=true \
-    charging_string.apply_v2=true
-
 # WFC Activation
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.gwfcactivation.disabled_carriers=1492
